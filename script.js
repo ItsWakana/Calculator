8a6de3c405f7f53c515ef87f9fa6ab5c7301fea5
MultiFlex <wakanawardrobe@gmail.com>
2022-04-20 17:29:41 +0100

    can now add multiple numbers together

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 
+const numbers = document.querySelectorAll('.numbers');
+const operations = document.querySelectorAll('.operate');
+const screen = document.getElementById('screen');
+const equals = document.querySelector('.equals');
+const clear = document.querySelector('.clear');
+
+let currentNumber = '';
+let arrayNums = [];
+let numberOps = [];
+let numSelection = '';
+
 function multiply(...nums) {
     let sum = nums.reduce((total, num) => total * num);
     screen.textContent = sum;
@@ -27,42 +38,59 @@ function operate(operator,...nums) {
     return operator(...nums);
 }
 
-const numbers = document.querySelectorAll('.numbers');
-const operation = document.querySelectorAll('.operate');
-const screen = document.getElementById('screen');
-const equals = document.querySelector('.equals');
-const clear = document.querySelector('.clear');
-let currentNumber;
-let arrayNums = [];
-let numberOps = [];
-
-
 function resetCalc() {
     arrayNums = [];
     numberOps = [];
     currentNumber = '';
+    numSelection = '';
     screen.textContent = '';
 }
-Array.from(numbers).forEach((number) => {
-    number.addEventListener('click', (e) => {
-        currentNumber = parseInt(e.target.id);
-        screen.textContent = `${currentNumber}`;
-    });
-});
 
-Array.from(operation).forEach((elem) => {
-    elem.addEventListener('click', (e) => {
-        if (e.target.id == "+" || "-" || "/" || "*") {
-            screen.textContent = e.target.id;
-            numberOps.push(e.target.id);
-            numberOps.length = 1;
-        }
-        arrayNums.push(currentNumber);
-});
-});
+function parseNumber(e) {
+    currentNumber += parseInt(e.target.id);
+    numSelection = parseInt(currentNumber);
+    screen.textContent = currentNumber;
+}
 
-equals.addEventListener('click', () => {
-    arrayNums.push(currentNumber);
+function parseOperation(e) {
+    if (e.target.id == "+" || "-" || "/" || "*") {
+        screen.textContent = e.target.id;
+        numberOps.push(e.target.id);
+        numberOps.length = 1;
+    }
+    
+        arrayNums.push(numSelection);
+    
+    if (arrayNums.length >= 2) {
+        operatorCalculation();
+    }
+    currentNumber = '';
+}
+
+function operatorCalculation() {
+    if (arrayNums.length < 2) {
+        arrayNums.push(numSelection);
+    }
+    if (numberOps.includes('+')) {
+        operate(add,...arrayNums);
+    } 
+    if (numberOps.includes('-')) {
+        operate(subtract,...arrayNums);
+    }
+    if (numberOps.includes('/')) {
+        operate(divide,...arrayNums);
+    }
+    if (numberOps.includes('*')) {
+        operate(multiply,...arrayNums);
+    }
+    if (arrayNums.length < 2) {
+    arrayNums = [];
+    }
+}
+
+function calculation() {
+    arrayNums.push(numSelection);
+
     if (numberOps.includes('+')) {
         operate(add,...arrayNums);
         numberOps = [];
@@ -80,6 +108,17 @@ equals.addEventListener('click', () => {
         numberOps = [];
     }
     arrayNums = [];
+
+}
+
+numbers.forEach((number) => {
+    number.addEventListener('click', parseNumber);
 });
+
+operations.forEach((operator) => {
+    operator.addEventListener('click', parseOperation);
+});
+
+equals.addEventListener('click', calculation);
 
 clear.addEventListener('click', resetCalc);
